f3f4e8d61588a1b8fb6fc1860f64241fe2479840
ivanbrennan <ivan.brennan@gmail.com>
2020-02-26 23:14:13 -0500

    vpn
    The initial motivation for writing this tiny program was to create a
    command-line utility I could call from my xmobarrc, since adding a
    plugin to xmobar is actually pretty cumbersome.
    
    All this currently does is check whether a vpn client is running, and if
    so, print out a padlock icon (in FontAwesome).

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,11 @@
 module Main where
 
+import System.Directory (doesPathExist)
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    vpnRunning <- doesPathExist "/proc/sys/net/ipv4/conf/tun0"
+    putStrLn (if vpnRunning then vpnIcon else "")
+  where
+    vpnIcon :: String
+    vpnIcon = "\xf023"
