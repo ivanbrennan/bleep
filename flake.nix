e7acfabff92b160ed766b95aa4f7b99480eadc6f
ivanbrennan <ivan.brennan@gmail.com>
2023-09-03 16:22:17 -0400

    0.1.5.0 -> 0.1.6.0 (base-4.17)

--- flake.nix
+++ flake.nix
@@ -1,4 +1,13 @@
 {
+  inputs = {
+    nixpkgs = {
+      type = "github";
+      owner = "NixOS";
+      repo = "nixpkgs";
+      ref = "nixos-unstable";
+    };
+  };
+
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
